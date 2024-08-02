b30acb413f7e5bafaf51fccfba883533bddf0872
Dricmoy Bhattacharjee <dricbhattacharjee@gmail.com>
2024-08-02 07:07:56 -0600

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 
 import { RefineThemes, useNotificationProvider } from "@refinedev/antd";
 import { Authenticated, ErrorComponent, Refine } from "@refinedev/core";
-import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
+
 import routerProvider, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -31,72 +31,72 @@ import "@refinedev/antd/dist/reset.css";
 const App = () => {
   return (
     <BrowserRouter>
-            <Refine
-              routerProvider={routerProvider}
-              dataProvider={dataProvider}
-              liveProvider={liveProvider}
-              notificationProvider={useNotificationProvider}
-              authProvider={authProvider}
-              resources={resources}
-              options={{
-                syncWithLocation: true,
-                warnWhenUnsavedChanges: true,
-                liveMode: "auto",
-                useNewQueryKeys: true,
-              }}
+      <Refine
+        routerProvider={routerProvider}
+        dataProvider={dataProvider}
+        liveProvider={liveProvider}
+        notificationProvider={useNotificationProvider}
+        authProvider={authProvider}
+        resources={resources}
+        options={{
+          syncWithLocation: true,
+          warnWhenUnsavedChanges: true,
+          liveMode: "auto",
+          useNewQueryKeys: true,
+        }}
+      >
+        <Routes>
+          <Route
+            element={
+              <Authenticated
+                key="authenticated-layout"
+                fallback={<CatchAllNavigate to="/login" />}
+              >
+                <Layout>
+                  <Outlet />
+                </Layout>
+              </Authenticated>
+            }
+          >
+            <Route index element={<DashboardPage />} />
+
+            <Route
+              path="/tasks"
+              element={
+                <TasksListPage>
+                  <Outlet />
+                </TasksListPage>
+              }
             >
-              <Routes>
-                <Route
-                  element={
-                    <Authenticated
-                      key="authenticated-layout"
-                      fallback={<CatchAllNavigate to="/login" />}
-                    >
-                      <Layout>
-                        <Outlet />
-                      </Layout>
-                    </Authenticated>
-                  }
-                >
-                  <Route index element={<DashboardPage />} />
+              <Route path="new" element={<TasksCreatePage />} />
+              <Route path="edit/:id" element={<TasksEditPage />} />
+            </Route>
 
-                  <Route
-                    path="/tasks"
-                    element={
-                      <TasksListPage>
-                        <Outlet />
-                      </TasksListPage>
-                    }
-                  >
-                    <Route path="new" element={<TasksCreatePage />} />
-                    <Route path="edit/:id" element={<TasksEditPage />} />
-                  </Route>
+            <Route path="/companies">
+              <Route index element={<CompanyListPage />} />
+              <Route path="new" element={<CompanyCreatePage />} />
+              <Route path="edit/:id" element={<CompanyEditPage />} />
+            </Route>
 
-                  <Route path="/companies">
-                    <Route index element={<CompanyListPage />} />
-                    <Route path="new" element={<CompanyCreatePage />} />
-                    <Route path="edit/:id" element={<CompanyEditPage />} />
-                  </Route>
+            <Route path="*" element={<ErrorComponent />} />
+          </Route>
 
-                  <Route path="*" element={<ErrorComponent />} />
-                </Route>
-
-                <Route
-                  element={
-                    <Authenticated
-                      key="authenticated-auth"
-                      fallback={<Outlet />}
-                    >
-                      <NavigateToResource resource="dashboard" />
-                    </Authenticated>
-                  }
-                >
-                  <Route path="/login" element={<LoginPage />} />
-                </Route>
-              </Routes>
-              <UnsavedChangesNotifier />
-              <DocumentTitleHandler />
-            </Refine>
+          <Route
+            element={
+              <Authenticated
+                key="authenticated-auth"
+                fallback={<Outlet />}
+              >
+                <NavigateToResource resource="dashboard" />
+              </Authenticated>
+            }
+          >
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
+        </Routes>
+        <UnsavedChangesNotifier />
+        <DocumentTitleHandler />
+      </Refine>
     </BrowserRouter>
   );
 };
